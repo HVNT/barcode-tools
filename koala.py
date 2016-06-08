cfe870c7f38c217f63d3eef79a430407b4aa4a3e
hvnt <hunterbrennick@gmail.com>
2016-06-08 19:07:56 -0400

    done

--- koala.py
+++ koala.py
@@ -1,14 +1,7 @@
 import string
 import sys
-import os
 import xlrd
-
-
-# from PIL import Image
-#
-# img = Image.new('RGB', (width, height))
-# img.putdata(my_list)
-# img.save('image.png')
+import os
 
 
 def col2num(col):
@@ -19,23 +12,42 @@ def col2num(col):
     return num
 
 
-def extract_images(sheet_path, img_col, id_col, row_offset):
-    book = xlrd.open_workbook(sheet_path)
-    sheet = book.sheet_by_index(0)    # get the first worksheet, can safely assume only one
+def get_target_files(dir_path, prefix):
+    directory = os.listdir(dir_path)
+    target_files = []
+    for file_name in directory:
+        if prefix in file_name:
+            target_files.append(file_name)
+    return target_files
 
+
+def rename_target_files(dir_path, target_files, sheet_path, id_col, row_offset):
+    book = xlrd.open_workbook(sheet_path)
+    sheet = book.sheet_by_index(0)  # get the first worksheet, can safely assume only one
+
+    img_ptr = 0
     for row_index in range(row_offset, sheet.nrows):
-        img = sheet.cell(row_index, img_col).value
-        id = sheet.cell(row_index, id_col).value
-        print "img", img
-        print "item id", id
+        product_id = str(sheet.cell(row_index, id_col).value)
+
+        try:
+            os.rename(dir_path + '/' + target_files[img_ptr], product_id + ".png")
+            print "[OK] Product " + str(product_id)
+        except (EnvironmentError, IndexError) as e:
+            print "[ERR] No file to rename for product: " + product_id
+
+        img_ptr += 1
 
 
 def main():
-    sheet_path = sys.argv[1]
-    img_col = col2num(sys.argv[2])
-    id_col = col2num(sys.argv[3])
-    row_offset = int(sys.argv[4])
-    extract_images(sheet_path, img_col, id_col, row_offset)
+    rename_dir_path = sys.argv[1]
+    rename_prefix = sys.argv[2]
+    sheet_path = sys.argv[3]
+    id_col = col2num(sys.argv[4])
+    row_offset = int(sys.argv[5])
+
+    target_files = get_target_files(rename_dir_path, rename_prefix)
+    rename_target_files(rename_dir_path, target_files, sheet_path, id_col, row_offset)
+
 
 if __name__ == "__main__":
     main()
